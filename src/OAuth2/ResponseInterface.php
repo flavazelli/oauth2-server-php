67ed2ef598be274b980a04575e10c20d13c85e11
Francis Lavazelli <francis@gradeslam.org>
2021-01-06 20:10:14 -0500

    test

--- src/OAuth2/ResponseInterface.php
+++ src/OAuth2/ResponseInterface.php
@@ -23,7 +23,7 @@ interface ResponseInterface
     /**
      * @param int $statusCode
      */
-    public function setStatusCode($statusCode);
+    public function setStatusCode(int $statusCode);
 
     /**
      * @param int    $statusCode
